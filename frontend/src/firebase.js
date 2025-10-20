f11c618a59d09e7d46d2416247e6a9324e89098b
Jayasurya Midde <jayanthjay751@gmail.com>
2025-10-20 23:45:08 +0530

    firebasekeys

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCms1UWtRpmUQV6PWnqOhkYNy7rhJ7jbLM",
-  authDomain: "mynotes-adbc7.firebaseapp.com",
-  projectId: "mynotes-adbc7",
-  storageBucket: "mynotes-adbc7.appspot.com",
-  messagingSenderId: "391349446495",
-  appId: "1:391349446495:web:73485e3ca419045e91f8ea",
-  measurementId: "G-QFR0Q5VQLH",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+
 };
 
 const app = initializeApp(firebaseConfig);
